650c7c03e4faab984603e1d6887bc8f37df311f5
jaketzu <jaakko.nevala007@gmail.com>
2025-10-23 00:04:18 +0300

    Valmis ohjelma (toivottavasti)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const notifications = new Map();
+const reminders = new Array();
 
 const reminderNameInput = document.getElementById("reminderName");
 const reminderIntervalInput = document.getElementById("reminderInterval");
@@ -10,24 +10,54 @@ document.getElementById("addReminder").addEventListener("click", () => {
     const reminderName = reminderNameInput.value;
     const reminderInterval = parseFloat(reminderIntervalInput.value);
 
-    if (result === "granted" && !isNaN(reminderInterval)) {
-      notifications.push(
-        new Notification(`${reminderName}`),
-        setInterval(() => {}, reminderInterval * 1000 * 60)
+    if (
+      result === "granted" &&
+      reminderName.trim !== "" &&
+      !isNaN(reminderInterval)
+    ) {
+      reminders.push(
+        setInterval(() => {
+          new Notification(`${reminderName}`);
+        }, reminderInterval * 1000 * 60)
       );
 
-      const li = document.createElement("li");
+      const div = document.createElement("div");
+      div.className = "reminder";
+      const reminderId = `${reminderName}${reminderInterval}${reminders.length}`;
+      div.id = reminderId;
 
-      const deleteButton = document.createElement("button");
-      deleteButton.textContent = "🗑️";
+      const header = document.createElement("h3");
+      header.textContent = `${reminderName}: ${reminderInterval} minutes`;
 
-      li.appendChild(deleteButton);
-      li.textContent = `${reminderName}: ${reminderInterval} minutes`;
+      const deleteButton = document.createElement("input");
+      deleteButton.type = "image";
+      deleteButton.src = "../images/trash.svg";
 
-      reminderList.appendChild(li);
+      div.appendChild(header);
+      div.appendChild(deleteButton);
+      reminderList.appendChild(div);
+
+      deleteButton.addEventListener("click", () => {
+        const indexOfReminder = Array.prototype.indexOf.call(
+          reminderList.children,
+          document.getElementById(reminderId)
+        );
+
+        clearInterval(reminders.at(indexOfReminder));
+        reminders.splice(indexOfReminder, 1);
+        document.getElementById(reminderId).remove();
+
+        console.log(
+          `Removed reminder ${reminderName} with id: ${reminderId}\n${reminders}`
+        );
+      });
 
       reminderNameInput.value = "";
       reminderIntervalInput.value = "";
+
+      console.log(
+        `Added reminder ${reminderName} with id: ${reminderId}\n${reminders}`
+      );
     }
   });
 });
